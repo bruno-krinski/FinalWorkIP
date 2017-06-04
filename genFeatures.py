00a25e7e77577cb6be66378da15357285adaf956
Bruno Alexandre Krinski <brunokrinski@outlook.com>
2017-06-04 17:23:49 -0300

    aaa

--- genFeatures.py
+++ genFeatures.py
@@ -22,7 +22,7 @@ def glcmFeaturesVector(images, images_path, output_file_name):
 
   method = ['contrast','dissimilarity','homogeneity',
             'energy', 'correlation', 'ASM']
-  
+
   for image in images:
     img = cv2.imread(images_path + "/" + image.name, 0)
     l = []
@@ -40,14 +40,14 @@ def glcmFeaturesVector(images, images_path, output_file_name):
       s += str(f) + " "
     s += "\n"
     output_file.writeString(s)
-      	
+
 
 
 def lbpFeaturesVector(images, images_path, output_file_name, method):
   output_file = File("features/" + output_file_name, "w+")
 
   lbp = LBP(1, method)
-    
+
   for image in images:
     img = cv2.imread(images_path + "/" + image.name, 0)
     image.features = lbp.describe(img)
@@ -61,14 +61,14 @@ def lbpFeaturesVector(images, images_path, output_file_name, method):
 
 def extractFeatures():
     file_name = 'training.txt'
-    images_path = '../TrainVal'
+    images_path = '../Trees'
 
     images = readImages(file_name)
-    
+
     method = ['default', 'ror', 'uniform', 'nri_uniform']
-    """for m in method:
-    	output_file_name = 'lbp_' + m + '_train.txt'  
-    	lbpFeaturesVector(images, images_path, output_file_name, m)
-    """
+    for m in method:
+        output_file_name = 'lbp_' + m + '_train.txt'
+        lbpFeaturesVector(images, images_path, output_file_name, m)
+
     output_file_name = 'glcm_train.txt'
     glcmFeaturesVector(images, images_path, output_file_name)
